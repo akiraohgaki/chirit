04fd6c7583e2a2bbd1205fa1d29df8e23c12226d
Akira Ohgaki <akiraohgaki@gmail.com>
2018-11-26 06:22:06 +0900

    Error handling

--- WebStorage.js
+++ WebStorage.js
@@ -10,8 +10,16 @@
 export default class WebStorage {
 
     constructor(type = 'local', prefix = '') {
-        // "type" should be "local" or "session"
-        this._storage = (type === 'local') ? window.localStorage : window.sessionStorage;
+        switch (type) {
+            case 'local':
+                this._storage = window.localStorage;
+                break;
+            case 'session':
+                this._storage = window.sessionStorage;
+                break;
+            default:
+                throw new Error(`Unknown storage type "${type}"`);
+        }
         this._prefix = prefix;
     }
 
