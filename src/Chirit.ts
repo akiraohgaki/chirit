43b1d242e5e524f1958dc7ac22459daf88eda816
Akira Ohgaki <akiraohgaki@gmail.com>
2020-01-01 18:38:38 +0000

    Module re-exporting

--- src/Chirit.ts
+++ src/Chirit.ts
@@ -4,26 +4,4 @@ import Handler from './Handler.js';
 import WebStorage from './WebStorage.js';
 import Utility from './Utility.js';
 
-export default class Chirit {
-
-    static get Component(): typeof Component {
-        return Component;
-    }
-
-    static get StateManager(): typeof StateManager {
-        return StateManager;
-    }
-
-    static get Handler(): typeof Handler {
-        return Handler;
-    }
-
-    static get WebStorage(): typeof WebStorage {
-        return WebStorage;
-    }
-
-    static get Utility(): typeof Utility {
-        return Utility;
-    }
-
-}
+export default {Component, StateManager, Handler, WebStorage, Utility};
