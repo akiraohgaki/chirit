2eca3e9cbe48de78e64c540182466777a1645a19
Akira Ohgaki <akiraohgaki@gmail.com>
2020-01-07 10:26:20 +0000

    Rewrite Component class

--- src/Component.ts
+++ src/Component.ts
@@ -1,16 +1,11 @@
-interface StateObject {
-    [key: string]: any;
-}
-
 export default class Component extends HTMLElement {
 
     static define(name: string, options?: ElementDefinitionOptions): void {
         window.customElements.define(name, this, options);
     }
 
-    private _state: StateObject;
+    private _state: object;
     private _shadowRoot: ShadowRoot | null;
-    private _template: HTMLTemplateElement;
     private _updateCount: number;
 
     constructor() {
@@ -18,81 +13,70 @@ export default class Component extends HTMLElement {
 
         this._state = {};
         this._shadowRoot = null;
-        this._template = document.createElement('template');
         this._updateCount = 0;
 
         this.initShadow();
-        //this.initTemplate();
         this.init();
-    }
-
-    set state(state: StateObject) {
-        this.setState(state);
-    }
-
-    get state(): StateObject {
-        return this.getState();
     }
 
     get contentRoot(): ShadowRoot | this {
         return this._shadowRoot || this.shadowRoot || this;
     }
 
-    initShadow(): void {
-        this.enableShadow();
+    set state(state: object) {
+        this.setState(state);
     }
 
-    /*initTemplate(): void {
-        this.importTemplate(document.createElement('template'));
-    }*/
+    get state(): object {
+        return this.getState();
+    }
 
-    setState(state: StateObject): void {
-        /*if (typeof state !== 'object' || state === null) {
-            throw new TypeError(`"${state}" is not an object`);
-        }*/
-
+    setState(state: object): void {
         const oldState = {...this._state};
         this._state = state;
         const newState = {...this._state};
-
-        this._stateChangedCallback(oldState, newState);
+        this.componentStateChangedCallback(oldState, newState);
+        if (this._updateCount) {
+            this._update();
+        }
+        //if (this._updateCount && JSON.stringify(oldState) !== JSON.stringify(newState)) {
+        //    this._update();
+        //}
     }
 
-    getState(): StateObject {
+    getState(): object {
         return this._state;
     }
 
-    setContent(content: string | Node): void {
-        if (typeof content === 'string') {
+    setContent(content: Node | string) {
+        if (content instanceof HTMLTemplateElement) {
+            content = content.content;
+        }
+        else if (typeof content === 'string') {
             const template = document.createElement('template');
             template.innerHTML = content;
             content = template.content;
         }
-
-        const oldContent = this.exportTemplate().content;
-        this._template.content.textContent = null;
-        this._template.content.appendChild(content);
-        const newContent = this.exportTemplate().content;
-
+        const oldContent = this.getContent();
         this.contentRoot.textContent = null;
-        this.contentRoot.appendChild(newContent.cloneNode(true));
-
-        this._contentChangedCallback(oldContent, newContent);
+        this.contentRoot.appendChild(content.cloneNode(true));
+        const newContent = this.getContent();
+        this.componentContentChangedCallback(oldContent, newContent);
     }
 
-    enableShadow(options: object = {}): void {
-        this._shadowRoot = this.attachShadow({mode: 'open', ...options});
+    getContent(): Node {
+        const documentFragment = document.createDocumentFragment();
+        if (this.contentRoot.hasChildNodes()) {
+            const childNodes = this.contentRoot.childNodes;
+            for (let i = 0; i < childNodes.length; i++) {
+                documentFragment.appendChild(childNodes[i].cloneNode(true));
+            }
+        }
+        return documentFragment;
     }
 
-    importTemplate(template: HTMLTemplateElement): void {
-        /*if (!(template instanceof HTMLTemplateElement)) {
-            throw new TypeError(`"${template}" is not a HTMLTemplateElement`);
-        }*/
-        this._template = template.cloneNode(true) as HTMLTemplateElement;
-    }
-
-    exportTemplate(): HTMLTemplateElement {
-        return this._template.cloneNode(true) as HTMLTemplateElement;
+    enableShadow(options: ShadowRootInit = {mode: 'open'}): void {
+        this._shadowRoot = this.attachShadow(options);
     }
 
     dispatch(type: string, data: object = {}): void {
@@ -103,53 +87,27 @@ export default class Component extends HTMLElement {
         }));
     }
 
-    update(state?: StateObject): void {
-        if (state !== undefined) {
-            this.setState(state);
-        }
-        else {
-            this._update();
-        }
+    protected initShadow(): void {
+        this.enableShadow();
+    }
+
+    protected init(): void {}
+
+    protected template(): Node | string {
+        return '';
+    }
+
+    protected render(): void {
+        this.setContent(this.template());
     }
 
     private _update(): void {
-        let content = this.render();
-        if (typeof content !== 'string' && !content) {
-            content = this.exportTemplate().content;
-        }
-
-        this.setContent(content);
-
-        this._updatedCallback();
+        this.render();
+        this._updateCount++;
+        this.componentUpdatedCallback();
     }
 
-    // Abstract methods
-
-    init(): void {}
-
-    render(): string | Node | void {}
-
-    // Lifecycle methods
-
-    static get componentObservedAttributes(): Array<string> {
-        return [];
-    }
-
-    componentAttributeChangedCallback(attributeName: string, oldValue: string, newValue: string, namespace: string): void {}
-
-    componentConnectedCallback(): void {}
-
-    componentDisconnectedCallback(): void {}
-
-    componentAdoptedCallback(oldDocument: Document, newDocument: Document): void {}
-
-    componentStateChangedCallback(oldState: StateObject, newState: StateObject): void {}
-
-    componentContentChangedCallback(oldContent: Node, newContent: Node): void {}
-
-    componentUpdatedCallback(): void {}
-
-    // Lifecycle methods in parent class
+    // Lifecycle callbacks
 
     static get observedAttributes(): Array<string> {
         return this.componentObservedAttributes;
@@ -173,32 +131,29 @@ export default class Component extends HTMLElement {
         this.componentDisconnectedCallback();
     }
 
-    adoptedCallback(oldDocument: Document, newDocument: Document): void {
+    adoptedCallback(oldDocument: Node, newDocument: Node): void {
         this.componentAdoptedCallback(oldDocument, newDocument);
         if (!this._updateCount) {
             this._update();
         }
     }
 
-    // Additional lifecycle methods
-
-    private _stateChangedCallback(oldState: StateObject, newState: StateObject): void {
-        this.componentStateChangedCallback(oldState, newState);
-        //if (this._updateCount && JSON.stringify(oldState) !== JSON.stringify(newState)) {
-        //    this.update();
-        //}
-        if (this._updateCount) {
-            this._update();
-        }
+    static get componentObservedAttributes(): Array<string> {
+        return [];
     }
 
-    private _contentChangedCallback(oldContent: Node, newContent: Node): void {
-        this.componentContentChangedCallback(oldContent, newContent);
-    }
+    protected componentAttributeChangedCallback(attributeName: string, oldValue: string, newValue: string, namespace: string): void {}
 
-    private _updatedCallback(): void {
-        this._updateCount++;
-        this.componentUpdatedCallback();
-    }
+    protected componentConnectedCallback(): void {}
+
+    protected componentDisconnectedCallback(): void {}
+
+    protected componentAdoptedCallback(oldDocument: Node, newDocument: Node): void {}
+
+    protected componentStateChangedCallback(oldState: object, newState: object): void {}
+
+    protected componentContentChangedCallback(oldContent: Node, newContent: Node): void {}
+
+    protected componentUpdatedCallback(): void {}
 
 }
