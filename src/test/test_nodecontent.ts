96622366d1b70556d95533479246c8192b3ca186
Akira Ohgaki <akiraohgaki@gmail.com>
2020-02-02 07:41:53 +0000

    Update test code

--- src/test/test_nodecontent.ts
+++ src/test/test_nodecontent.ts
@@ -1,36 +1,55 @@
 import Chirit from '../chirit.js';
 
 export default function() {
-    const template = document.createElement('template');
-    template.innerHTML = `
-        <form id="nodecontent-form">
-        <textarea id="nodecontent-contentsrc" style="width: 800px; height: 400px;">
-        <header>
-        <h1>header</h1>
-        </header>
-        <article>
-        <p data-attr1="1" data-attr2="2" required>paragraph</p>
-        <!-- comment -->
-        text
-        </article>
+    const main = document.getElementById('main') as Element;
+    main.innerHTML = `
+        <textarea id="nodecontent-src" style="width: 600px; height: 300px;">
+        <ul data-attr1="1" data-attr2="2" data-attr3="3">
+        <li>list1</li>
+        <li>list2</li>
+        <li>list3</li>
+        </ul>
         </textarea>
-        <br>
-        <button>Update</button>
-        </form>
+        <div id="nodecontent-methods">
+        <button data-method="update">Update</button>
+        <button data-method="set">Set</button>
+        <button data-method="get">Get</button>
+        <button data-method="clear">Clear</button>
+        </div>
         <div id="nodecontent-content"></div>
     `;
-    document.body.appendChild(template.content);
 
-    const form = document.getElementById('nodecontent-form') as HTMLFormElement;
-    const contentSrc = document.getElementById('nodecontent-contentsrc') as HTMLTextAreaElement;
-    const content = document.getElementById('nodecontent-content') as HTMLDivElement;
+    const methods = document.getElementById('nodecontent-methods') as Element;
+    const src = document.getElementById('nodecontent-src') as HTMLTextAreaElement;
+    const content = document.getElementById('nodecontent-content') as Element;
 
     const nodeContent = new Chirit.NodeContent(content);
     console.log(nodeContent.target);
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        nodeContent.update(contentSrc.value);
-    }, false);
+    methods.addEventListener('click', (event) => {
+        const target = event.target as Element;
+        const element = target.closest('[data-method]');
+        if (element) {
+            const method = element.getAttribute('data-method') as string;
+            switch (method) {
+                case 'update': {
+                    nodeContent.update(src.value || '');
+                    break;
+                }
+                case 'set': {
+                    nodeContent.set(src.value || '');
+                    break;
+                }
+                case 'get': {
+                    console.log(nodeContent.get());
+                    break;
+                }
+                case 'clear': {
+                    nodeContent.clear();
+                    break;
+                }
+            }
+        }
+    });
 
 }
