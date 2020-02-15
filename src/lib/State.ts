037837f3599ed59b3c53ba5d195c35ea5879f3dc
Akira Ohgaki <akiraohgaki@gmail.com>
2020-02-16 01:10:40 +0900

    Add observer

--- src/lib/State.ts
+++ src/lib/State.ts
@@ -1,25 +1,48 @@
 import {Dictionary} from './common.js';
 
 export interface StateHandler {
-    (data: Dictionary<any>, currentData: Dictionary<any>): Dictionary<any>;
+    (data: Dictionary<any>, state: Dictionary<any>): Dictionary<any>;
+}
+
+export interface StateObserver {
+    (state: Dictionary<any>): void;
 }
 
 export default class State {
 
-    private _data: Dictionary<any>;
+    private _state: Dictionary<any>;
     private _handler: StateHandler;
+    private _observerCollection: Set<StateObserver>;
 
-    constructor(data?: Dictionary<any>, handler?: StateHandler) {
-        this._data = data || {};
+    constructor(state?: Dictionary<any>, handler?: StateHandler, observers?: Iterable<StateObserver>) {
+        this._state = state || {};
         this._handler = handler || this._defaultHandler;
+        this._observerCollection = new Set(observers);
     }
 
-    get data(): Dictionary<any> {
-        return this._data;
+    getState(): Dictionary<any> {
+        return this._state;
+    }
+
+    setHandler(handler: StateHandler): void {
+        this._handler = handler;
+    }
+
+    addObserver(observer: StateObserver): void {
+        this._observerCollection.add(observer);
+    }
+
+    deleteObserver(observer: StateObserver): boolean {
+        return this._observerCollection.delete(observer);
     }
 
     async update(data: Dictionary<any>): Promise<void> {
-        this._data = await Promise.resolve(this._handler(data, this._data));
+        this._state = await Promise.resolve(this._handler(data, this._state));
+        if (this._observerCollection.size) {
+            for (const observer of this._observerCollection) {
+                observer(this._state);
+            }
+        }
     }
 
     private _defaultHandler(data: Dictionary<any>): Dictionary<any> {
