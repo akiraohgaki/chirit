4a3dceb12c81ee085e6f126f7edf24413cfbebf4
Akira Ohgaki <akiraohgaki@gmail.com>
2020-02-20 21:10:38 +0000

    Update chirit.ts

--- src/chirit.ts
+++ src/chirit.ts
@@ -1,6 +1,7 @@
 import Component from './lib/Component.js';
 import NodeContent from './lib/NodeContent.js';
 import Store from './lib/Store.js';
+import Observable from './lib/Observable.js';
 import StateManager from './lib/StateManager.js';
 import State from './lib/State.js';
 import WebStorage from './lib/WebStorage.js';
@@ -10,6 +11,7 @@ export default {
     Component,
     NodeContent,
     Store,
+    Observable,
     StateManager,
     State,
     WebStorage,
