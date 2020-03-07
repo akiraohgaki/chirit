ef2cbd5e21bb2f163771040096a4e27ad6a85f3f
Akira Ohgaki <akiraohgaki@gmail.com>
2020-03-08 00:39:33 +0900

    Rewrite State class

--- src/lib/State.ts
+++ src/lib/State.ts
@@ -1,54 +1,40 @@
 import {Dictionary} from './common.js';
 
-export interface StateHandler {
-    (data: Dictionary<any>, state: Dictionary<any>): Dictionary<any>;
-}
-
-export interface StateObserver {
-    (state: Dictionary<any>): void;
-}
-
 export default class State {
 
     private _state: Dictionary<any>;
-    private _handler: StateHandler;
-    private _observerCollection: Set<StateObserver>;
+    private _stateProxy: Dictionary<any>;
+    private _stateJson: string;
+    private _stateJsonCollection: Map<string, string>;
 
-    constructor(state?: Dictionary<any>, handler?: StateHandler, observers?: Iterable<StateObserver>) {
-        this._state = state || {};
-        this._handler = handler || this._defaultHandler;
-        this._observerCollection = new Set(observers);
+    constructor(state: Dictionary<any>, handler?: ProxyHandler<Dictionary<any>>) {
+        this._state = state;
+        this._stateProxy = new Proxy(this._state, handler || {});
+        this._stateJson = JSON.stringify(this._state);
+        this._stateJsonCollection = new Map();
     }
 
-    get state(): Dictionary<any> {
-        return this._state;
-    }
-
-    setHandler(handler: StateHandler): void {
-        this._handler = handler;
-    }
-
-    addObserver(observer: StateObserver): void {
-        this._observerCollection.add(observer);
-    }
-
-    removeObserver(observer: StateObserver): void {
-        this._observerCollection.delete(observer);
-    }
-
-    async update(data: Dictionary<any>): Promise<void> {
-        this._state = await Promise.resolve(this._handler(data, this._state));
-        this._notifyObservers();
-    }
-
-    private _notifyObservers(): void {
-        for (const observer of this._observerCollection) {
-            observer(this._state);
+    commit(state: Dictionary<any>, tag?: string): void {
+        Object.assign(this._stateProxy, state);
+        this._stateJson = JSON.stringify(this._state);
+        if (tag) {
+            this._stateJsonCollection.set(tag, this._stateJson);
         }
     }
 
-    private _defaultHandler(data: Dictionary<any>): Dictionary<any> {
-        return data;
+    checkout(tag?: string): Dictionary<any> | null {
+        if (tag) {
+            const stateJson = this._stateJsonCollection.get(tag);
+            if (stateJson) {
+                return JSON.parse(stateJson);
+            }
+            return null;
+        }
+        return JSON.parse(this._stateJson);
+    }
+
+    tags(): Array<string> {
+        return Array.from(this._stateJsonCollection.keys());
     }
 
 }
