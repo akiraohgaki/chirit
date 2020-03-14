90061bcbfe3d506fd0ca85d9322939366e24d58a
Akira Ohgaki <akiraohgaki@gmail.com>
2020-03-14 13:33:58 +0900

    Rewrite State class

--- src/lib/State.ts
+++ src/lib/State.ts
@@ -2,39 +2,57 @@ import {Dictionary} from './common.js';
 
 export default class State {
 
-    private _state: Dictionary<any>;
-    private _stateProxy: Dictionary<any>;
-    private _stateJson: string;
-    private _stateJsonCollection: Map<string, string>;
+    private _json: string;
+    private _jsonCollection: Map<string, string>;
+    private _handler: ProxyHandler<Dictionary<any>>;
 
-    constructor(state: Dictionary<any>, handler?: ProxyHandler<Dictionary<any>>) {
-        this._state = state;
-        this._stateProxy = new Proxy(this._state, handler || {});
-        this._stateJson = JSON.stringify(this._state);
-        this._stateJsonCollection = new Map();
+    constructor(state: Dictionary<any> = {}, handler: ProxyHandler<Dictionary<any>> = {}) {
+        this._json = JSON.stringify(state);
+        this._jsonCollection = new Map();
+        this._handler = handler;
     }
 
-    commit(state: Dictionary<any>, tag?: string): void {
-        Object.assign(this._stateProxy, state);
-        this._stateJson = JSON.stringify(this._state);
-        if (tag) {
-            this._stateJsonCollection.set(tag, this._stateJson);
+    commit(state: Dictionary<any>): void {
+        this._merge(this.clone(), state);
+    }
+
+    clone(): Dictionary<any> {
+        return JSON.parse(this._json);
+    }
+
+    createVersion(version: string): void {
+        this._jsonCollection.set(version, this._json);
+    }
+
+    deleteVersion(version: string): boolean {
+        return this._jsonCollection.delete(version);
+    }
+
+    cloneFromVersion(version: string): Dictionary<any> | null {
+        const json = this._jsonCollection.get(version);
+        if (json) {
+            return JSON.parse(json);
         }
+        return null;
     }
 
-    checkout(tag?: string): Dictionary<any> | null {
-        if (tag) {
-            const stateJson = this._stateJsonCollection.get(tag);
-            if (stateJson) {
-                return JSON.parse(stateJson);
-            }
-            return null;
+    revertToVersion(version: string): boolean {
+        const state = this.cloneFromVersion(version);
+        if (state) {
+            this._merge({}, state);
+            return true;
         }
-        return JSON.parse(this._stateJson);
+        return false;
     }
 
-    tags(): Array<string> {
-        return Array.from(this._stateJsonCollection.keys());
+    versions(): Array<string> {
+        return Array.from(this._jsonCollection.keys());
+    }
+
+    private _merge(oldState: Dictionary<any>, newState: Dictionary<any>): void {
+        const proxy = new Proxy(oldState, this._handler);
+        Object.assign(proxy, newState);
+        this._json = JSON.stringify(proxy);
     }
 
 }
