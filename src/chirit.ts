51bcf4aeea80736d72c17a6cb98caa8c6ad543d0
Akira Ohgaki <akiraohgaki@gmail.com>
2020-03-30 12:11:42 +0900

    Delete StateManager class and State class

--- src/chirit.ts
+++ src/chirit.ts
@@ -2,8 +2,6 @@ import Component from './lib/Component.js';
 import NodeContent from './lib/NodeContent.js';
 import Store from './lib/Store.js';
 import Observable from './lib/Observable.js';
-import StateManager from './lib/StateManager.js';
-import State from './lib/State.js';
 import WebStorage from './lib/WebStorage.js';
 import Utility from './lib/Utility.js';
 
@@ -12,8 +10,6 @@ export default {
     NodeContent,
     Store,
     Observable,
-    StateManager,
-    State,
     WebStorage,
     Utility
 };
