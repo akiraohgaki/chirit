8b8d901c605a381d1498a21a85e8c2264c75942a
Akira Ohgaki <akiraohgaki@gmail.com>
2020-04-11 04:24:52 +0900

    Delete Store class

--- src/chirit.ts
+++ src/chirit.ts
@@ -1,6 +1,5 @@
 import Component from './lib/Component.js';
 import NodeContent from './lib/NodeContent.js';
-import Store from './lib/Store.js';
 import Observable from './lib/Observable.js';
 import WebStorage from './lib/WebStorage.js';
 import Utility from './lib/Utility.js';
@@ -8,7 +7,6 @@ import Utility from './lib/Utility.js';
 export default {
     Component,
     NodeContent,
-    Store,
     Observable,
     WebStorage,
     Utility
