a5696ca0c75600144df51d8624768fa403bffc76
Akira Ohgaki <akiraohgaki@gmail.com>
2020-04-13 14:53:21 +0900

    Rewrite Component class

--- src/lib/Component.ts
+++ src/lib/Component.ts
@@ -4,17 +4,17 @@ import NodeContent, {NodeContentData} from './NodeContent.js';
 export default class Component extends HTMLElement {
 
     private _shadowRoot: ShadowRoot | null;
-    private _state: Dictionary<any>;
-    private _updateLockCount: number;
-    private _updatedCount: number;
+    private _updateTimeoutId: number | undefined;
+    private _updateDelay: number;
+    private _updateCount: number;
 
     constructor() {
         super();
 
         this._shadowRoot = this.initShadow();
-        this._state = this.initState();
-        this._updateLockCount = 0;
-        this._updatedCount = 0;
+        this._updateTimeoutId = undefined;
+        this._updateDelay = 100;
+        this._updateCount = 0;
     }
 
     static define(name: string, options?: ElementDefinitionOptions): void {
@@ -25,37 +25,6 @@ export default class Component extends HTMLElement {
         return this._shadowRoot || this.shadowRoot || this;
     }
 
-    set state(state: Dictionary<any>) {
-        this.setState(state);
-    }
-
-    get state(): Dictionary<any> {
-        return this.getState();
-    }
-
-    setState(state: Dictionary<any>): void {
-        const oldState = {...this._state};
-        this._state = state;
-        const newState = {...this._state};
-
-        this._updateLockCount++;
-        this.componentStateChangedCallback(oldState, newState);
-        this._updateLockCount--;
-
-        if (!this._updateLockCount && this._updatedCount) {
-            this._update();
-        }
-        //if (!this._updateLockCount && this._updatedCount
-        //    && !Utility.isSameObject(oldState, newState)
-        //) {
-        //    this._update();
-        //}
-    }
-
-    getState(): Dictionary<any> {
-        return this._state;
-    }
-
     dispatch(type: string, detail: Dictionary<any> = {}): boolean {
         return this.contentRoot.dispatchEvent(new CustomEvent(type, {
             detail: detail,
@@ -64,11 +33,18 @@ export default class Component extends HTMLElement {
         }));
     }
 
-    private _update(): void {
+    update(): void {
         if (this.isConnected) {
-            this.render();
-            this._updatedCount++;
-            this.componentUpdatedCallback();
+            if (this._updateTimeoutId !== undefined) {
+                window.clearTimeout(this._updateTimeoutId);
+            }
+            this._updateTimeoutId = window.setTimeout(() => {
+                window.clearTimeout(this._updateTimeoutId);
+                this._updateTimeoutId = undefined;
+                this.render();
+                this._updateCount++;
+                this.updatedCallback();
+            }, this._updateDelay);
         }
     }
 
@@ -76,10 +52,6 @@ export default class Component extends HTMLElement {
 
     protected initShadow(): ShadowRoot | null {
         return this.attachShadow({mode: 'open'});
-    }
-
-    protected initState(): Dictionary<any> {
-        return {};
     }
 
     protected render(): void {
@@ -93,54 +65,18 @@ export default class Component extends HTMLElement {
 
     // Lifecycle methods
 
-    static get observedAttributes(): Array<string> {
-        return this.componentObservedAttributes;
-    }
-
-    attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null, namespace: string | null): void {
-        this._updateLockCount++;
-        this.componentAttributeChangedCallback(name, oldValue, newValue, namespace);
-        this._updateLockCount--;
-
-        if (!this._updateLockCount && this._updatedCount
-            && oldValue !== newValue
-        ) {
-            this._update();
+    attributeChangedCallback(_name: string, oldValue: string | null, newValue: string | null): void {
+        if (this._updateCount && oldValue !== newValue) {
+            this.update();
         }
     }
 
     connectedCallback(): void {
-        this._updateLockCount++;
-        this.componentConnectedCallback();
-        this._updateLockCount--;
-
-        if (!this._updateLockCount && !this._updatedCount) {
-            this._update();
+        if (!this._updateCount) {
+            this.update();
         }
     }
 
-    disconnectedCallback(): void {
-        this.componentDisconnectedCallback();
-    }
-
-    adoptedCallback(oldDocument: Document, newDocument: Document): void {
-        this.componentAdoptedCallback(oldDocument, newDocument);
-    }
-
-    static get componentObservedAttributes(): Array<string> {
-        return [];
-    }
-
-    componentAttributeChangedCallback(_name: string, _oldValue: string | null, _newValue: string | null, _namespace: string | null): void {}
-
-    componentConnectedCallback(): void {}
-
-    componentDisconnectedCallback(): void {}
-
-    componentAdoptedCallback(_oldDocument: Document, _newDocument: Document): void {}
-
-    componentStateChangedCallback(_oldState: Dictionary<any>, _newState: Dictionary<any>): void {}
-
-    componentUpdatedCallback(): void {}
+    updatedCallback(): void {}
 
 }
