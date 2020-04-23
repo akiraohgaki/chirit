7173cd2dd992a1bcc104f0f2a19e2e6e70095199
Akira Ohgaki <akiraohgaki@gmail.com>
2020-04-23 23:55:56 +0000

    Update demo

--- src/demo/store/index.ts
+++ src/demo/store/index.ts
@@ -1,15 +1,3 @@
 import Store from './Store.js';
 
-interface AlternativeWindow extends Window {
-    _store: Store;
-}
-
-declare var window: AlternativeWindow;
-
-if (!(window._store instanceof Store)) {
-    window._store = new Store();
-}
-
-const store = window._store;
-
-export default store;
+export default new Store();
