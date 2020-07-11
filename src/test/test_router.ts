ff8338642181380788adac441d9f7eb87f674016
Akira Ohgaki <akiraohgaki@gmail.com>
2020-07-12 07:48:07 +0900

    Update test

--- src/test/test_router.ts
+++ src/test/test_router.ts
@@ -1,12 +1,14 @@
 import {Router} from '../chirit.js';
 
 export default function() {
-    const prefix = '/test';
     const main = document.getElementById('main') as Element;
     main.innerHTML = `
         <div id="router-links">
-        <a href="${prefix}/router/dummy">${prefix}/router/dummy</a><br>
-        <a href="${prefix}/router/0-1">${prefix}/router/0-1</a><br>
+        <a href="${window.location.href}?k=v#/test/router/dummy">${window.location.href}?k=v#/test/router/dummy</a><br>
+        <a href="?k=v#/test/router/dummy">?k=v#/test/router/dummy</a><br>
+        <a href="#/test/router/dummy">#/test/router/dummy</a><br>
+        <a href="/test/router/dummy">/test/router/dummy</a><br>
+        <a href="/test/router/0-1">/test/router/0-1</a><br>
         <a href="2-3">2-3</a><br>
         <a href="./">./</a><br>
         <a href="../">../</a>
@@ -15,22 +17,23 @@ export default function() {
 
     const links = document.getElementById('router-links') as Element;
 
-    const router = new Router('history');
+    const router = new Router('hash');
 
     console.log(router.type);
 
-    router.setRoute(`${prefix}/router/dummy`, () => {
+    router.setRoute('/test/router/dummy', () => {
         console.log('dummy');
     });
-    router.setRoute(`${prefix}/router/:param1-(?<param2>.+)`, (params) => {
+    router.setRoute('/test/router/:param1-(?<param2>.+)', (params) => {
         console.log(window.location.href);
         console.log(params);
     });
-    router.setRoute('.*', () => {
+    router.setRoute('.*', (params) => {
         console.log(window.location.href);
+        console.log(params);
     });
 
-    router.removeRoute(`${prefix}/router/dummy`);
+    router.removeRoute('/test/router/dummy');
 
     router.navigate(window.location.href);
 
