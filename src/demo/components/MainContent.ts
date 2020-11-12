604c3ecdf8e3d85aa244604e858f5c3b9abdff84
Akira Ohgaki <akiraohgaki@gmail.com>
2020-11-13 05:45:27 +0900

    Update demo file

--- src/demo/components/MainContent.ts
+++ src/demo/components/MainContent.ts
@@ -6,7 +6,7 @@ export default class MainContent extends Component {
 
     constructor() {
         super();
-        store.state.searchResult.subscribe(this.update.bind(this));
+        store.state.searchResult.subscribe(this.render.bind(this));
     }
 
     template(): string {
