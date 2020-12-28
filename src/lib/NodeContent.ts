593159e1be8f74bf5a452437f6464fe77ff171e7
Akira Ohgaki <akiraohgaki@gmail.com>
2020-12-28 17:08:56 +0900

    Change method names

--- src/lib/NodeContent.ts
+++ src/lib/NodeContent.ts
@@ -14,10 +14,10 @@ export default class NodeContent<T extends Node> {
 
     update(content: NodeContentData): void {
         if (content instanceof Document || content instanceof DocumentFragment) {
-            this._updateChildNodes(this._container, content);
+            this._patchChildNodes(this._container, content);
         }
         else {
-            this._updateChildNodes(this._container, this._createDocumentFragment(content));
+            this._patchChildNodes(this._container, this._createDocumentFragment(content));
         }
     }
 
@@ -46,71 +46,69 @@ export default class NodeContent<T extends Node> {
         return documentFragment;
     }
 
-    private _updateChildNodes(oldParent: Node, newParent: Node): void {
-        const oldChildNodes = Array.from(oldParent.childNodes);
-        const newChildNodes = Array.from(newParent.childNodes);
-        const maxLength = Math.max(oldChildNodes.length, newChildNodes.length);
+    private _patchChildNodes(original: Node, diff: Node): void {
+        const originalChildNodes = Array.from(original.childNodes);
+        const diffChildNodes = Array.from(diff.childNodes);
+        const maxLength = Math.max(originalChildNodes.length, diffChildNodes.length);
 
         for (let i = 0; i < maxLength; i++) {
-            this._updateChildNode(
-                oldParent,
-                oldChildNodes[i] ?? null,
-                newChildNodes[i] ?? null
+            this._patchNode(
+                original,
+                originalChildNodes[i] ?? null,
+                diffChildNodes[i] ?? null
             );
         }
     }
 
-    private _updateChildNode(parent: Node, oldChild: Node | null, newChild: Node | null): void {
-        if (oldChild && !newChild) {
-            parent.removeChild(oldChild);
+    private _patchNode(parent: Node, original: Node | null, diff: Node | null): void {
+        if (original && !diff) {
+            parent.removeChild(original);
         }
-        else if (!oldChild && newChild) {
-            parent.appendChild(newChild.cloneNode(true));
+        else if (!original && diff) {
+            parent.appendChild(diff.cloneNode(true));
         }
-        else if (oldChild && newChild) {
-            if (oldChild.nodeType === newChild.nodeType
-                && oldChild.nodeName === newChild.nodeName
+        else if (original && diff) {
+            if (original.nodeType === diff.nodeType
+                && original.nodeName === diff.nodeName
             ) {
-                // Current child is same node type and/or same tag name
-                if (oldChild instanceof Element && newChild instanceof Element) {
-                    // Current child is Element like HTMLElement, SVGElement
-                    this._updateAttributes(oldChild, newChild);
-                    // Update recursively
-                    this._updateChildNodes(oldChild, newChild);
+                if (original instanceof Element && diff instanceof Element) {
+                    // The Element will be like HTMLElement, SVGElement
+                    this._patchAttributes(original, diff);
+                    // Continue patching recursively
+                    this._patchChildNodes(original, diff);
                 }
-                else if (oldChild instanceof CharacterData && newChild instanceof CharacterData) {
-                    // Current child is CharacterData like Text, Comment, ProcessingInstruction
-                    if (oldChild.nodeValue !== newChild.nodeValue) {
-                        oldChild.nodeValue = newChild.nodeValue;
+                else if (original instanceof CharacterData && diff instanceof CharacterData) {
+                    // The CharacterData will be like Text, Comment, ProcessingInstruction
+                    if (original.nodeValue !== diff.nodeValue) {
+                        original.nodeValue = diff.nodeValue;
                     }
                 }
                 else {
-                    // Current child is any other node type like DocumentType
-                    parent.replaceChild(newChild.cloneNode(true), oldChild);
+                    // Any other node type like DocumentType, just replace it for now
+                    parent.replaceChild(diff.cloneNode(true), original);
                 }
             }
             else {
-                // Current child is different node type or different tag name
-                parent.replaceChild(newChild.cloneNode(true), oldChild);
+                parent.replaceChild(diff.cloneNode(true), original);
             }
         }
     }
 
-    private _updateAttributes(oldParent: Element, newParent: Element): void {
-        const oldAttributes = Array.from(oldParent.attributes);
-        const newAttributes = Array.from(newParent.attributes);
+    private _patchAttributes(original: Element, diff: Element): void {
+        const originalAttributes = Array.from(original.attributes);
+        const diffAttributes = Array.from(diff.attributes);
 
-        for (const attribute of oldAttributes) {
-            if (!newParent.hasAttribute(attribute.name)) {
-                oldParent.removeAttribute(attribute.name);
+        for (const attribute of originalAttributes) {
+            if (!diff.hasAttribute(attribute.name)) {
+                original.removeAttribute(attribute.name);
             }
         }
 
-        for (const attribute of newAttributes) {
-            if (!oldParent.hasAttribute(attribute.name)
-                || oldParent.getAttribute(attribute.name) !== attribute.value
+        for (const attribute of diffAttributes) {
+            if (!original.hasAttribute(attribute.name)
+                || original.getAttribute(attribute.name) !== attribute.value
             ) {
-                oldParent.setAttribute(attribute.name, attribute.value);
+                original.setAttribute(attribute.name, attribute.value);
             }
         }
     }
