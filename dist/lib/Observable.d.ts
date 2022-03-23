7ce7178a70d54e598f2058aa8e5a94664e0b600f
Akira Ohgaki <akiraohgaki@gmail.com>
2022-03-24 02:05:56 +0900

    Cleanup

--- dist/lib/Observable.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Observer } from './types.js';
-export default class Observable<T = any> {
-    private _observerCollection;
-    constructor();
-    subscribe(observer: Observer<T>): void;
-    unsubscribe(observer: Observer<T>): void;
-    notify(value: T): void;
-}
