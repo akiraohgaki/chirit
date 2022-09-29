c705969fc49b34f630c663441625cb7491896668
Akira Ohgaki <akiraohgaki@gmail.com>
2022-09-29 04:17:01 +0000

    Add test code

---
+++ src/tests/dom.ts
@@ -0,0 +1,12 @@
+import { JSDOM } from 'https://jspm.dev/jsdom';
+
+const jsdom = new JSDOM(
+  `<!DOCTYPE html><html><head></head><body></body></html>`,
+  {
+    url: 'https://example.com/',
+    referrer: 'https://example.org/',
+    contentType: 'text/html',
+  },
+);
+
+globalThis.document = jsdom.window.document;
