364c922d8e85b25056f6bb317623ef517bba903b
Akira Ohgaki <akiraohgaki@gmail.com>
2022-10-16 23:45:53 +0000

    Update test code

--- src/tests/CustomElement.test.ts
+++ src/tests/CustomElement.test.ts
@@ -3,7 +3,7 @@ import util from './util.ts';
 import CustomElement from '../CustomElement.ts';
 
 Deno.test('CustomElement', { sanitizeResources: false, sanitizeOps: false }, async (t) => {
-  let customElement: CustomElement;
+  let testElement: CustomElement;
 
   let counter = 0;
 
@@ -60,91 +60,91 @@ Deno.test('CustomElement', { sanitizeResources: false, sanitizeOps: false }, asy
   };
 
   await t.step('define()', () => {
-    TestElement.define('custom-element');
+    TestElement.define('test-element');
     // observedAttributes will fire
 
-    assertStrictEquals(util.globalThis.customElements.get('custom-element') !== undefined, true);
+    assertStrictEquals(util.globalThis.customElements.get('test-element') !== undefined, true);
     assertStrictEquals(counter, 1);
   });
 
   await t.step('constructor()', () => {
     // From constructor()
-    customElement = new TestElement();
+    testElement = new TestElement();
 
-    assertInstanceOf(customElement, TestElement);
-    assertInstanceOf(customElement, CustomElement);
-    assertStrictEquals(customElement.updateCounter, 0);
+    assertInstanceOf(testElement, TestElement);
+    assertInstanceOf(testElement, CustomElement);
+    assertStrictEquals(testElement.updateCounter, 0);
     assertStrictEquals(counter, 1);
-    assertStrictEquals(customElement.getAttribute('test'), null);
+    assertStrictEquals(testElement.getAttribute('test'), null);
 
     // From document.createElement()
-    customElement = util.globalThis.document.createElement('custom-element') as CustomElement;
+    testElement = util.globalThis.document.createElement('test-element') as CustomElement;
 
-    assertInstanceOf(customElement, TestElement);
-    assertInstanceOf(customElement, CustomElement);
-    assertStrictEquals(customElement.updateCounter, 0);
+    assertInstanceOf(testElement, TestElement);
+    assertInstanceOf(testElement, CustomElement);
+    assertStrictEquals(testElement.updateCounter, 0);
     assertStrictEquals(counter, 1);
-    assertStrictEquals(customElement.getAttribute('test'), null);
+    assertStrictEquals(testElement.getAttribute('test'), null);
 
     // From HTML tag
-    util.globalThis.document.body.innerHTML = '<custom-element test="0"></custom-element>';
+    util.globalThis.document.body.innerHTML = '<test-element test="0"></test-element>';
     // attributeChangedCallback() and connectedCallback() will fire
-    customElement = util.globalThis.document.querySelector('custom-element') as CustomElement;
+    testElement = util.globalThis.document.querySelector('test-element') as CustomElement;
 
-    assertInstanceOf(customElement, TestElement);
-    assertInstanceOf(customElement, CustomElement);
-    assertStrictEquals(customElement.updateCounter, 1);
+    assertInstanceOf(testElement, TestElement);
+    assertInstanceOf(testElement, CustomElement);
+    assertStrictEquals(testElement.updateCounter, 1);
     assertStrictEquals(counter, 5);
-    assertStrictEquals(customElement.getAttribute('test'), '0');
+    assertStrictEquals(testElement.getAttribute('test'), '0');
   });
 
   await t.step('attributeChangedCallback()', async () => {
     // Should work with debounce way
-    customElement.setAttribute('test', '1');
+    testElement.setAttribute('test', '1');
     await sleep();
 
-    assertStrictEquals(customElement.getAttribute('test'), '1');
-    assertStrictEquals(customElement.updateCounter, 2);
+    assertStrictEquals(testElement.getAttribute('test'), '1');
+    assertStrictEquals(testElement.updateCounter, 2);
     assertStrictEquals(counter, 8);
 
-    customElement.setAttribute('test', '2');
-    customElement.setAttribute('test', '3');
-    customElement.setAttribute('test', '4');
+    testElement.setAttribute('test', '2');
+    testElement.setAttribute('test', '3');
+    testElement.setAttribute('test', '4');
     await sleep();
 
-    assertStrictEquals(customElement.getAttribute('test'), '4');
-    assertStrictEquals(customElement.updateCounter, 3);
+    assertStrictEquals(testElement.getAttribute('test'), '4');
+    assertStrictEquals(testElement.updateCounter, 3);
     assertStrictEquals(counter, 13);
 
     // If the value is the same it will not update
-    customElement.setAttribute('test', '4');
+    testElement.setAttribute('test', '4');
     await sleep();
 
-    assertStrictEquals(customElement.getAttribute('test'), '4');
-    assertStrictEquals(customElement.updateCounter, 3);
+    assertStrictEquals(testElement.getAttribute('test'), '4');
+    assertStrictEquals(testElement.updateCounter, 3);
     assertStrictEquals(counter, 14);
 
     // update() should not fire for unobserved attribute
-    customElement.setAttribute('unobserved', '0');
+    testElement.setAttribute('unobserved', '0');
     await sleep();
 
-    assertStrictEquals(customElement.getAttribute('unobserved'), '0');
-    assertStrictEquals(customElement.updateCounter, 3);
+    assertStrictEquals(testElement.getAttribute('unobserved'), '0');
+    assertStrictEquals(testElement.updateCounter, 3);
     assertStrictEquals(counter, 14);
 
     // Check if errorCallback() is fire
-    customElement.setAttribute('test', 'error');
+    testElement.setAttribute('test', 'error');
     await sleep();
 
-    assertStrictEquals(customElement.getAttribute('test'), 'error');
-    assertStrictEquals(customElement.updateCounter, 4);
+    assertStrictEquals(testElement.getAttribute('test'), 'error');
+    assertStrictEquals(testElement.updateCounter, 4);
     assertStrictEquals(counter, 18);
   });
 
   await t.step('disconnectedCallback()', () => {
-    util.globalThis.document.body.removeChild(customElement);
+    util.globalThis.document.body.removeChild(testElement);
 
-    assertStrictEquals(customElement.updateCounter, 4);
+    assertStrictEquals(testElement.updateCounter, 4);
     assertStrictEquals(counter, 19);
   });
 
@@ -152,11 +152,11 @@ Deno.test('CustomElement', { sanitizeResources: false, sanitizeOps: false }, asy
     const iframe = util.globalThis.document.createElement('iframe');
     iframe.srcdoc = '<!DOCTYPE html><html><head></head><body></body></html>';
     util.globalThis.document.body.appendChild(iframe);
-    iframe.contentWindow?.document.body.appendChild(customElement);
+    iframe.contentWindow?.document.body.appendChild(testElement);
     // adoptedCallback() and connectedCallback() will fire
     await sleep();
 
-    assertStrictEquals(customElement.updateCounter, 5);
+    assertStrictEquals(testElement.updateCounter, 5);
     assertStrictEquals(counter, 24);
   });
 
