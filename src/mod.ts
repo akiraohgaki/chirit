19dd2dbb2d4c9e1a81d4a234d84f25046f05263d
Akira Ohgaki <akiraohgaki@gmail.com>
2022-10-21 00:02:08 +0900

    Update mod.ts

--- src/mod.ts
+++
@@ -1,8 +0,0 @@
-export { default as Component } from './Component.ts';
-export { default as CustomElement } from './CustomElement.ts';
-export { default as ElementAttributesProxy } from './ElementAttributesProxy.ts';
-export { default as NodeContent } from './NodeContent.ts';
-export { default as Observable } from './Observable.ts';
-export { default as ObservableValue } from './ObservableValue.ts';
-export { default as Router } from './Router.ts';
-export { default as WebStorage } from './WebStorage.ts';
