1b37310feb5c67c28f1e46e2caa125f7e82d61d4
Akira Ohgaki <akiraohgaki@gmail.com>
2022-12-09 05:55:52 +0000

    Update test code

--- tests/createComponent.test.ts
+++ tests/createComponent.test.ts
@@ -1,11 +1,53 @@
-import { assertStrictEquals } from 'std/testing/asserts.ts';
+import { assertInstanceOf, assertNotInstanceOf, assertStrictEquals } from 'std/testing/asserts.ts';
 import dom from './dom.ts';
 import createComponent from '../src/createComponent.ts';
+import Component from '../src/Component.ts';
+import ObservableValue from '../src/ObservableValue.ts';
 
 Deno.test('createComponent', { sanitizeResources: false, sanitizeOps: false }, async (t) => {
-  await t.step('createComponent()', () =>{
-    const CustomComponent = createComponent('custom-component', {});
+  const observableValue = new ObservableValue(0);
 
+  let counter = 0;
+
+  const sleep = (time: number) => {
+    return new Promise((resolve) => {
+      dom.globalThis.setTimeout(resolve, time);
+    });
+  };
+
+  await t.step('createComponent()', async () => {
+    const CustomComponent = createComponent('custom-component', {
+      observedAttributes: ['test'],
+      observedObjects: [observableValue],
+      init: (context) => {
+        context.updatedCallback = () => {
+          counter++;
+          console.log(counter);
+        };
+      },
+      template: (context) => {
+        return `<span>${context.attrs.test}</span><span>${observableValue.get()}</span>`;
+      },
+    });
+
+    dom.globalThis.document.body.innerHTML = '<custom-component test="0"></custom-component>';
+    const customComponent = dom.globalThis.document.querySelector('custom-component') as Component;
+    const container = customComponent.content.container as ShadowRoot;
+
+    // This component type is Function instead of Component class
+    assertInstanceOf(CustomComponent, Function);
+    assertNotInstanceOf(CustomComponent, Component);
     assertStrictEquals(dom.globalThis.customElements.get('custom-component') !== undefined, true);
+    assertStrictEquals(container.innerHTML, '<span>0</span><span>0</span>');
+    assertStrictEquals(counter, 1);
+
+    // Should be update correctly
+    counter = 0;
+    customComponent.attrs.test = '1';
+    observableValue.set(2);
+    await sleep(200);
+
+    assertStrictEquals(container.innerHTML, '<span>1</span><span>2</span>');
+    assertStrictEquals(counter, 1);
   });
 });
