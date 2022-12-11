2d3660d787489c90ae45b0a6d1f0a98257496ef8
Akira Ohgaki <akiraohgaki@gmail.com>
2022-12-11 17:10:38 +0000

    Update createComponent

--- src/createComponent.ts
+++ src/createComponent.ts
@@ -2,47 +2,52 @@ import type { NodeContentData } from './types.ts';
 
 import Component from './Component.ts';
 
-export default function createComponent(name: string, options?: {
+export default function createComponent<T = Component>(name: string, options?: {
   observedAttributes?: Array<string>;
-  observedObjects?: Array<unknown>;
-  init?: { (context: Component): void };
-  template?: { (context: Component): NodeContentData };
-}): typeof CustomComponent {
+  init?: { (context: T): void };
+  connected?: { (context: T): void };
+  disconnected?: { (context: T): void };
+  template?: { (context: T): NodeContentData };
+}): T {
   const CustomComponent = class extends Component {
     static override get observedAttributes(): Array<string> {
-      return options?.observedAttributes ?? super.observedAttributes;
+      return (options?.observedAttributes && Array.isArray(options.observedAttributes))
+        ? options.observedAttributes
+        : super.observedAttributes;
     }
 
     constructor() {
       super();
 
       if (options?.init && typeof options.init === 'function') {
-        options.init(this);
+        options.init(this as unknown as T);
       }
     }
 
     override connectedCallback(): void {
       super.connectedCallback();
 
-      if (options?.observedObjects) {
-        this.observe(...options.observedObjects);
+      if (options?.connected && typeof options.connected === 'function') {
+        options.connected(this as unknown as T);
       }
     }
 
     override disconnectedCallback(): void {
-      if (options?.observedObjects) {
-        this.unobserve(...options.observedObjects);
+      if (options?.disconnected && typeof options.disconnected === 'function') {
+        options.disconnected(this as unknown as T);
       }
 
       super.disconnectedCallback();
     }
 
     override template(): NodeContentData {
-      return (options?.template && typeof options.template === 'function') ? options.template(this) : super.template();
+      return (options?.template && typeof options.template === 'function')
+        ? options.template(this as unknown as T)
+        : super.template();
     }
   };
 
   CustomComponent.define(name);
 
-  return CustomComponent;
+  return CustomComponent as T;
 }
