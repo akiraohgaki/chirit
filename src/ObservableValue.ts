4cd5a8931ae69e4b16747c37fad5c75029f9d03c
Akira Ohgaki <akiraohgaki@gmail.com>
2024-07-25 11:10:13 +0900

    ObservableValue resettable

--- src/ObservableValue.ts
+++ src/ObservableValue.ts
@@ -1,12 +1,19 @@
 import Observable from './Observable.ts';
 
 export default class ObservableValue<T> extends Observable<T> {
+  #initialValue: T;
   #value: T;
 
   constructor(value: T) {
     super();
 
+    this.#initialValue = value;
     this.#value = value;
+  }
+
+  reset(): void {
+    this.#value = this.#initialValue;
+    this.notify();
   }
 
   set(value: T): void {
