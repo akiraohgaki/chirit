b4adf8b144976e7a663c7ceecadeab2db09a46eb
Akira Ohgaki <akiraohgaki@gmail.com>
2024-09-04 12:56:10 +0900

    Code formatting

--- tests/e2e/state.spec.ts
+++ tests/e2e/state.spec.ts
@@ -1,22 +1,22 @@
-import { expect, test } from "@playwright/test";
+import { expect, test } from '@playwright/test';
 
-const initialState = "";
-const updatedState = "text";
+const initialState = '';
+const updatedState = 'text';
 
-test.describe("/state", () => {
+test.describe('/state', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("/state");
+    await page.goto('/state');
 
     await page.locator('[data-action="init"]').click();
   });
 
-  test("Initialization", async ({ page }) => {
-    await expect(page.locator("[data-log]")).toHaveText([
-      "action: init",
+  test('Initialization', async ({ page }) => {
+    await expect(page.locator('[data-log]')).toHaveText([
+      'action: init',
     ]);
   });
 
-  test("Methods", async ({ page }) => {
+  test('Methods', async ({ page }) => {
     await page.locator('[data-action="clear-log"]').click();
 
     await page.locator('[data-action="method-get"]').click();
@@ -31,40 +31,40 @@ test.describe("/state", () => {
     await page.locator('[data-action="method-notify"]').click();
     await page.locator('[data-action="method-set"]').click();
 
-    await expect(page.locator("[data-log]")).toHaveText([
-      "action: method-get",
+    await expect(page.locator('[data-log]')).toHaveText([
+      'action: method-get',
       initialState,
 
-      "action: method-notify",
+      'action: method-notify',
 
-      "action: method-subscribe",
+      'action: method-subscribe',
 
-      "action: method-notify",
+      'action: method-notify',
       `observer1: ${initialState}`,
       `observer2: ${initialState}`,
       `observer3: ${initialState}`,
 
-      "action: method-set",
+      'action: method-set',
       `observer1: ${updatedState}`,
       `observer2: ${updatedState}`,
       `observer3: ${updatedState}`,
 
-      "action: method-get",
+      'action: method-get',
       updatedState,
 
-      "action: method-reset",
+      'action: method-reset',
       `observer1: ${initialState}`,
       `observer2: ${initialState}`,
       `observer3: ${initialState}`,
 
-      "action: method-get",
+      'action: method-get',
       initialState,
 
-      "action: method-unsubscribe",
+      'action: method-unsubscribe',
 
-      "action: method-notify",
+      'action: method-notify',
 
-      "action: method-set",
+      'action: method-set',
     ]);
   });
 });
