b65951add091a731b376d7497f543a8408d8d933
Akira Ohgaki <akiraohgaki@gmail.com>
2024-09-15 05:36:53 +0900

    Add dump.sh

---
+++ dump.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DUMP_FILE='./dump.txt'
+
+dump() {
+  echo '// ----------------------------------------------------------------' >>"${DUMP_FILE}"
+  echo "// Below is the content of the file ${1}" >>"${DUMP_FILE}"
+  echo '// ----------------------------------------------------------------' >>"${DUMP_FILE}"
+  cat "${1}" >>"${DUMP_FILE}"
+  echo '' >>"${DUMP_FILE}"
+}
+
+rm -rf "${DUMP_FILE}"
+
+dump ./mod.ts
+dump ./types.ts
+
+for file in ./src/*; do
+  dump "${file}"
+done
