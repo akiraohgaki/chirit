fdc7904a52192e047ab0ae5936d9d7c4ec201ebd
Akira Ohgaki <akiraohgaki@gmail.com>
2024-09-15 23:38:32 +0900

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -3,15 +3,18 @@
 DUMP_FILE='./dump.txt'
 
 dump() {
-  echo '// ----------------------------------------------------------------' >>"${DUMP_FILE}"
-  echo "// Below is the content of the file ${1}" >>"${DUMP_FILE}"
-  echo '// ----------------------------------------------------------------' >>"${DUMP_FILE}"
+  echo '----------------------------------------------------------------' >>"${DUMP_FILE}"
+  echo "Below is the content of the file ${1}" >>"${DUMP_FILE}"
+  echo '----------------------------------------------------------------' >>"${DUMP_FILE}"
   cat "${1}" >>"${DUMP_FILE}"
   echo '' >>"${DUMP_FILE}"
 }
 
 rm -rf "${DUMP_FILE}"
 
+dump ./README.md
+dump ./LICENSE
+dump ./deno.json
 dump ./mod.ts
 dump ./types.ts
 
