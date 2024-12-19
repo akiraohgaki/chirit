1634db2c90047ac8189c81af519d3051b3d9eac2
Akira Ohgaki <akiraohgaki@gmail.com>
2024-12-19 03:07:18 +0000

    Update test code

--- tests/unit/dom.test.ts
+++ tests/unit/dom.test.ts
@@ -3,10 +3,7 @@ import { assertStrictEquals } from '@std/assert';
 import { dom } from '../../dom.ts';
 
 Deno.test('dom', async (t) => {
-  await t.step('globalThis', () => {
+  await t.step('globalThis.HTMLElement', () => {
     assertStrictEquals('HTMLElement' in dom.globalThis, true);
-    assertStrictEquals('structuredClone' in dom.globalThis, true);
-    assertStrictEquals('localStorage' in dom.globalThis, true);
-    assertStrictEquals('sessionStorage' in dom.globalThis, true);
   });
 });
