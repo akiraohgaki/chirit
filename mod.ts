c71463c6f472068c8f1a27e5ee67304c96757e46
Akira Ohgaki <akiraohgaki@gmail.com>
2024-12-31 05:20:23 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -4,11 +4,6 @@
 
 export * from './src/types.ts';
 
-export { default as compareValues } from './src/compareValues.ts';
-export { default as createComponent } from './src/createComponent.ts';
-export { default as css } from './src/css.ts';
-export { default as html } from './src/html.ts';
-
 export { default as Component } from './src/Component.ts';
 export { default as CustomElement } from './src/CustomElement.ts';
 export { default as ElementAttributesProxy } from './src/ElementAttributesProxy.ts';
@@ -18,3 +13,10 @@ export { default as Router } from './src/Router.ts';
 export { default as State } from './src/State.ts';
 export { default as Store } from './src/Store.ts';
 export { default as WebStorage } from './src/WebStorage.ts';
+
+export { default as compareValues } from './src/compareValues.ts';
+export { default as createComponent } from './src/createComponent.ts';
+export { default as css } from './src/css.ts';
+export { default as html } from './src/html.ts';
+
+export { default as dom } from './src/dom.ts';
