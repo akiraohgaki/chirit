fc93ab66b8083a841e4940545fde73065fe38493
Akira Ohgaki <akiraohgaki@gmail.com>
2025-01-13 22:20:28 +0900

    Update test code

--- tests/unit/utils/debounce.test.ts
+++ tests/unit/utils/debounce.test.ts
@@ -19,4 +19,22 @@ Deno.test('debounce', async (t) => {
 
     assertEquals(logs, [2, 4]);
   });
+
+  await t.step('time-consuming debounced function', async () => {
+    const logs: Array<number> = [];
+
+    const debouncedFunc = debounce(async (value: number) => {
+      await sleep(150);
+      logs.push(value);
+    }, 50);
+
+    debouncedFunc(1);
+    debouncedFunc(2);
+    await sleep(100);
+    debouncedFunc(3);
+    debouncedFunc(4);
+    await sleep(100);
+
+    assertEquals(logs, [2]);
+  });
 });
