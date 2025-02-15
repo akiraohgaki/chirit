ae119527dce5085377eaca868dba45e422d40933
Akira Ohgaki <akiraohgaki@gmail.com>
2025-02-16 00:36:59 +0900

    Update test code

--- tests/unit/dom.test.ts
+++ tests/unit/dom.test.ts
@@ -1,13 +1,13 @@
-import { assert } from '@std/assert';
+import { assert, assertNotStrictEquals } from '@std/assert';
 
 import { dom } from '../../mod.ts';
 
 Deno.test('dom', async (t) => {
-  await t.step('has globalThis', () => {
-    assert('globalThis' in dom);
+  await t.step('globalThis', () => {
+    assertNotStrictEquals(dom.globalThis, globalThis);
   });
 
-  await t.step('has globalThis.HTMLElement', () => {
+  await t.step('globalThis.HTMLElement', () => {
     assert('HTMLElement' in dom.globalThis);
     assert(!('HTMLElement' in globalThis));
   });
